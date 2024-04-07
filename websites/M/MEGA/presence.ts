b5fb04bf606dd366d15d58fb83c72fd0229e4ac3
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2024-04-07 13:54:29 +0200

    feat(MEGA): rewrite presence

--- websites/M/MEGA/presence.ts
+++ websites/M/MEGA/presence.ts
@@ -3,143 +3,225 @@ const presence = new Presence({
 	}),
 	browsingTimestamp = Math.floor(Date.now() / 1000);
 
-let menu: Element, file: string;
-
 presence.on("UpdateData", async () => {
-	const presenceData: PresenceData = {
-			largeImageKey:
-				"https://cdn.rcd.gg/PreMiD/websites/M/MEGA/assets/logo.png",
-			startTimestamp: browsingTimestamp,
-		},
-		[privacy, buttons] = await Promise.all([
+	let presenceData: PresenceData = {
+		largeImageKey: "https://cdn.rcd.gg/PreMiD/websites/M/MEGA/assets/logo.png",
+		startTimestamp: browsingTimestamp,
+	};
+	const [privacy, buttons] = await Promise.all([
 			presence.getSetting<boolean>("privacy"),
 			presence.getSetting<boolean>("buttons"),
 		]),
-		{ pathname, hostname, href } = document.location;
+		{ pathname, hostname, href } = document.location,
+		ioStaticPages: Record<string, PresenceData> = {
+			"/": {
+				details: "Viewing the homepage",
+			},
+			"/business": {
+				details: privacy
+					? "Viewing a page"
+					: "Viewing MEGA's business solutions",
+			},
+			"/bug-bounty": {
+				details: privacy
+					? "Viewing a page"
+					: "Viewing MEGA's bug bountry program",
+			},
+			"/contact": {
+				details: privacy ? "Viewing a page" : "Viewing MEGA's contact page",
+			},
+			"/pricing": {
+				details: "Viewing pricing",
+			},
+			"/storage": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Storage",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/chatandmeetings": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Chat and meetings",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/syncing": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Syncing",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/megabackup": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Backups",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/share": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Sharing",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/mediafiles": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Media files",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/objectstorage": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "Object storage",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/vpn": {
+				details: privacy ? "Viewing a product" : "Viewing product",
+				state: "VPN",
+				buttons: [
+					{
+						label: "View Product",
+						url: href,
+					},
+				],
+			},
+			"/individuals": {
+				details: privacy ? "Viewing a solution" : "Viewing solution for",
+				state: "Individuals",
+				buttons: [
+					{
+						label: "View Solution",
+						url: href,
+					},
+				],
+			},
+			"/freelancers": {
+				details: privacy ? "Viewing a solution" : "Viewing solution for",
+				state: "Freelancers",
+				buttons: [
+					{
+						label: "View Solution",
+						url: href,
+					},
+				],
+			},
+			"/small-business": {
+				details: privacy ? "Viewing a solution" : "Viewing solution for",
+				state: "Small buisness",
+				buttons: [
+					{
+						label: "View Solution",
+						url: href,
+					},
+				],
+			},
+		},
+		nzStaticPages: Record<string, PresenceData> = {
+			"/": {
+				details: "Viewing the homepage",
+			},
+		};
 
-	if (privacy) presenceData.details = "Browsing";
-	else if (hostname.includes("mega.io")) {
-		menu = document.querySelector("[class='pages-menu submenu-link clickurl']");
-		if (pathname === "/" || pathname.includes("/startpage"))
-			presenceData.details = "Home page";
-		else if (menu) {
-			presenceData.details = menu.getAttribute("href");
-			presenceData.buttons = [
-				{
-					label: "View Features",
-					url: href,
-				},
-			];
+	switch (hostname) {
+		case "mega.io": {
+			switch (pathname) {
+				default: {
+					for (const [path, data] of Object.entries(ioStaticPages)) {
+						if (pathname !== path && pathname.startsWith(path))
+							presenceData = { ...presenceData, ...data };
+						else if (pathname === path)
+							presenceData = { ...presenceData, ...data };
+					}
+				}
+			}
+			break;
 		}
-	} else if (hostname.includes("mega.nz")) {
-		menu =
-			document.querySelector(".top-menu-item.active") ??
-			document.querySelector(".btn-myfiles.js-lpbtn.active");
+		case "mega.nz": {
+			switch (true) {
+				case pathname.includes("/fm"): {
+					const activeFolder = document
+						.querySelector(".content-area.js-myfiles-panel")
+						.querySelector(".selected");
+					switch (true) {
+						case pathname.includes("/shares"): {
+							break;
+						}
+						case pathname.includes("/out-shares"): {
+							break;
+						}
+						case pathname.includes("/public-links"): {
+							break;
+						}
+						case pathname.includes("/file-requests"): {
+							break;
+						}
+						case pathname.includes("/photos"): {
+							break;
+						}
+						case pathname.includes("/chat"): {
+							break;
+						}
+						case pathname.includes("/devices"): {
+							break;
+						}
 
-		if (pathname === "/" || pathname.includes("/startpage"))
-			presenceData.details = "Home page";
-		else if (pathname.includes("/file/")) {
-			const extension = document.querySelector(
-				"[class='extension']"
-			).textContent;
-			presenceData.details = `Viewing a ${extension} file`;
-			presenceData.state = ` (${document
-				.querySelector("[class='file-size-block']")
-				.textContent.trim()}) ${
-				document.querySelector("[class='filename']").textContent
-			}${extension}`;
-			presenceData.buttons = [
-				{
-					label: "View File",
-					url: href,
-				},
-			];
-		} else if (pathname.includes("/account")) {
-			presenceData.details = `Viewing ${document
-				.querySelector("[class='content-panel account active']")
-				.querySelector("[class*='active']")
-				.firstElementChild.textContent.trim()} Settings`;
-		} else if (pathname.includes("/fm/") && !pathname.includes("/fm/chat")) {
-			const selected =
-				document
-					.querySelector('[class*="ui-selected"]')
-					?.getAttribute("title") ??
-				document
-					.querySelector('[class*="data-block-view ui-selected"]')
-					?.getAttribute("title");
-			if (selected) {
-				const title = selected.replace(/[0-9]* .B/gm, "");
-				file = selected.replace(title, "");
-				if (file) file = `(${file})`;
-				presenceData.details = "Viewing file";
-				presenceData.state = `${file} ${title}`;
-			} else if (
-				!document
-					.querySelector(
-						"#bodyel > section.media-viewer-container.theme-dark-forced"
-					)
-					?.className.includes("hidden")
-			) {
-				presenceData.details = "Viewing file";
-				presenceData.state = document.querySelector(
-					"[class='file-name']"
-				).textContent;
-			} else {
-				presenceData.details = `Viewing ${
-					document.querySelector(".btn-galleries.js-lpbtn.active")
-						?.textContent ??
-					document.querySelector(".btn-myfiles.js-lpbtn.active")?.textContent ??
-					"Dashboard"
-				}`;
+						default: {
+							switch (true) {
+								case !!activeFolder?.textContent: {
+									presenceData.details = privacy
+										? "Viewing files"
+										: `Drive - ${activeFolder?.textContent}`;
+									break;
+								}
+							}
+						}
+					}
+
+					break;
+				}
+				default: {
+					for (const [path, data] of Object.entries(nzStaticPages)) {
+						if (pathname !== path && pathname.startsWith(path))
+							presenceData = { ...presenceData, ...data };
+						else if (pathname === path)
+							presenceData = { ...presenceData, ...data };
+					}
+				}
 			}
-		} else if (pathname.includes("/fm/chat")) {
-			const chats = document.querySelector(
-					"#fmholder > div.main-blur-block > div.fm-main.default > div.section.conversations > div > div.fm-right-files-block.in-chat > div.conversation-panels > div > div > div.chat-topic-block.chat-topic-block > div.chat-topic-info > div.chat-topic-text > span:nth-child(1) > span"
-				),
-				contacts =
-					document
-						?.querySelector('[class*="contacts-navigation"]')
-						?.querySelector('[class*="active"]') ??
-					document?.querySelector('[class*=" active groupchat public"]');
-			if (chats) {
-				presenceData.details = "Viewing chat";
-				presenceData.state = chats.textContent;
-			} else if (contacts) presenceData.details = contacts.textContent;
-			else presenceData.details = "Viewing All chats";
-		} else if (document.querySelector("[class*=' ui-selected']")) {
-			const titleNode = document.querySelector("[class*=' ui-selected']")
-				.childNodes[1];
-			if (
-				document
-					.querySelector("[class*=' ui-selected']")
-					.innerHTML.includes("folder")
-			) {
-				presenceData.details = "Viewing folder";
-				presenceData.state = titleNode.textContent;
-			} else {
-				presenceData.details = "Viewing file";
-				presenceData.state = `${titleNode.textContent} (Size: ${document
-					.querySelector("[class*=' ui-selected']")
-					.getAttribute("title")
-					.replace(titleNode.textContent, "")
-					.trim()})`;
-			}
-		} else if (
-			document.querySelector("[class='fm-breadcrumbs-block']") &&
-			document.querySelector(
-				'[class="next-arrow sprite-fm-mono icon-arrow-right icon16"]'
-			)
-		) {
-			presenceData.details = "Viewing contents of folder";
-			presenceData.state = document
-				.querySelector("[class='fm-breadcrumbs-block']")
-				.lastChild.textContent.trim();
-		} else if (menu) presenceData.details = `Viewing ${menu.textContent}`;
-		else if (pathname === "/business")
-			presenceData.details = "Viewing Business";
+			break;
+		}
 	}
-
+	if (privacy && presenceData.state) delete presenceData.state;
 	if (!buttons) delete presenceData.buttons;
+
 	if (presenceData.details) presence.setActivity(presenceData);
 	else presence.setActivity();
 });
